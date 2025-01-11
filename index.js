f8c70d1ab044f0eaeff298225c28d96fc8233d92
TomatoXpert <thomasadaimy0@gmail.com>
2025-01-11 18:55:27 +0000

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Watching over Gamer Media, Listening to your requests, Powered By RTX];
 
 
 let currentIndex = 0;
